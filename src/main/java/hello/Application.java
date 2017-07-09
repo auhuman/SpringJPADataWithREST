a6afbb7e8a69b8938fbeb3ef8279bc2a37b93114
Arunkumar Mathiyazhagan <auhuman@gmail.com>
2017-07-09 03:26:44 -0700

    Inital Commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by srinath on 7/8/2017.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
